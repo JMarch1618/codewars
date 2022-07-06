c701677e6e0548ae46c4dc31b733f7c08c5ccb68
jmarch <jmarch1618@gmail.com>
2022-07-05 23:21:07 -0500

    2022-0705 DailyCodeWars
    2022-0705 CodeWars JS

---
+++ 2022_0705 CodeWars (Function 1 - hello world).js
@@ -0,0 +1,107 @@
+
+DELETE THIS 
+DELETE THIS 
+DELETE THIS 
+ALT + Z WILL WRAP THE TEXT
+
+/* 
+NAME OF PROBLEM: XXXXX
+LINK TO PROBLEM: https://www.codewars.com/kata/523b4ff7adca849afe000035
+LINK TO PROBLEM: https://www.codewars.com/kata/523b4ff7adca849afe000035/train/javascript
+KYU LEVEL: 8KYU
+
+SOLVED: YES
+*/
+
+
+/*
+INSTRUCTIONS: 
+// Write a function "greet" that returns "hello world!"
+*/
+
+/*
+DESCRIPTION:
+Make a simple function called greet that returns the most-famous "hello world!".
+*/
+
+/*
+STYLE POINTS:
+Sure, this is about as easy as it gets. But how clever can you be to create the most creative hello world you can think of? What is a "hello world" solution you would want to show your friends?
+
+
+/*
+EXAMPLE:
+XXXXXX
+*/
+
+
+/*
+NOTES
+XXXXXX
+*/
+
+
+// MY SOLUTION
+// Write a function "greet" that returns "hello world!"
+function greet (){
+	return "hello world!"}
+
+// let wordSelected = prompt(`Enter a phase: `, `Hello World`);
+console.log(greet());
+
+
+
+
+/* 
+SAMPLE TEST
+const assert = require("chai").assert;
+
+describe("Testing function", function() {
+  it("Is it a function?", function() {
+    assert.strictEqual(typeof greet, "function", "greet should be a function");
+  });
+  it("Correct return-value?", function() {
+    assert.strictEqual(greet(), "hello world!");
+  });
+});
+*/
+
+
+
+
+
+
+
+// Best Solution
+function greet() {
+    return "hello world!";
+    }
+
+// 2nd Best Solution
+const greet = () => "hello world!";
+
+// 3rd Best Solution
+XXXX
+
+// 4th Best Solution
+XXX
+
+
+// nth Best Solution
+XXX
+
+// nth Best Solution
+XXX
+
+// nth Best Solution
+XXX
+
+// nth Best Solution
+XXX
+
+// nth Best Solution
+XXX
+
+// nth Best Solution
+XXX
+
